1b04477e2a8e0d46b152d9ac28af5fbf7eb16674
SMohanaPrriya <s.mohanaprriya@gmail.com>
2024-02-15 23:33:22 -0500

    key-value pair - base of hashtable datatype creation

---
+++ src/main/java/datastructures/hashtables/KeyValuePair.java
@@ -0,0 +1,23 @@
+package datastructures.hashtables;
+
+public class KeyValuePair {
+    private int key;
+    private int value;
+
+
+    //getter methods
+    public int getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    //setter method
+    public void setKeyValuePair(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
