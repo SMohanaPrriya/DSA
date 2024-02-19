25147fb8c44e7f1af71c38d7a1ff2573f9324e1a
SMohanaPrriya <s.mohanaprriya@gmail.com>
2024-02-19 18:54:52 -0500

    Stack implementation using Arrays

--- src/main/java/datastructures/Stack/Arrays/StackUsingArrays.java
+++ src/main/java/datastructures/Stack/Arrays/StackUsingArrays.java
@@ -1,5 +1,49 @@
 package datastructures.Stack.Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StackUsingArrays {
+    List<String> stack = new ArrayList<>();
+
+    String peek() {
+        return stack.get(stack.size()-1);
+    }
+
+    void push(String item) {
+        stack.add(item);
+    }
+
+    String pop() {
+        if (stack.isEmpty()) {
+            return "Stack is empty";
+        }
+        int top = stack.size()-1;
+        String returnValue = stack.get(top);
+        stack.remove(top);
+        return returnValue;
+    }
+
+    void printStack() {
+        int top = stack.size()-1;
+        System.out.printf("Stack: %s", stack.get(top));
+        for(int i = stack.size()-2; i >= 0; i--) {
+            System.out.printf(" <-- %s ", stack.get(i));
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        StackUsingArrays newStack = new StackUsingArrays();
+        newStack.push("There!");
+        newStack.push("Hi");
+        newStack.push("Hello");
+        newStack.printStack();
+        System.out.println(newStack.peek());
+        System.out.println(newStack.pop());
+        System.out.println(newStack.peek());
+        newStack.printStack();
+    }
+
 
 }
