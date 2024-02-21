c8422334b13a448fb1482e32d7d8f280f7f67af6
SMohanaPrriya <s.mohanaprriya@gmail.com>
2024-02-20 19:17:25 -0500

    BST Deletion

--- src/main/java/datastructures/Trees/BinarySearchTree.java
+++ src/main/java/datastructures/Trees/BinarySearchTree.java
@@ -71,6 +71,75 @@ public class BinarySearchTree {
         return null;
     }
 
+    void remove(int value) {
+        if(isNull(getRoot())) {
+            return;
+        }
+        TreeNode currentNode = getRoot();
+        TreeNode parentNode = null;
+
+        while(currentNode!=null) {
+            if(currentNode.getValue()>value) {
+                parentNode = currentNode;
+                currentNode = currentNode.getLeft();
+            } else if (currentNode.getValue()<value) {
+                parentNode = currentNode;
+                currentNode = currentNode.getRight();
+            } else {
+
+                //CASE1: No Right Child
+                if(isNull(currentNode.getRight())) {
+                    if(isNull(parentNode)) { //deletion of root node
+                        root = currentNode.getLeft();
+                    } else {
+                        //if parent > current, make CURRENT LEFT child as a LEFT child of parent
+                        if(parentNode.getValue() > currentNode.getValue()) {
+                            parentNode.setLeft(currentNode.getLeft());
+                        }
+                        //if parent < current, make CURRENT LEFT child as a RIGHT child of parent
+                        if(parentNode.getValue() < currentNode.getValue()) {
+                            parentNode.setRight(currentNode.getLeft());
+                        }
+                    }
+                    //CASE2: Right child which doesn't have a left child
+                } else if (isNull(currentNode.getRight().getLeft())) {
+                    if(isNull(parentNode)) {
+                        root = currentNode.getRight();
+                    } else {
+                        // if current < parent, make current's right child a left child of parent
+                        if (currentNode.getValue() < parentNode.getValue()) {
+                            parentNode.setLeft(currentNode.getRight());
+                        }
+                        // if current > parent, make current's right child a right child of parent
+                        else if (currentNode.getValue() > parentNode.getValue()) {
+                            parentNode.setRight(currentNode.getRight());
+                        }
+                    }
+                } // Option 3: RIGHT CHILD WHICH HAVE LEFT CHILD
+                else {
+                    if (parentNode == null) {
+                        // save reference of left and right nodes of the root
+                        TreeNode leftNode = root.getLeft();
+                        TreeNode rightNode = root.getRight();
+                        // root.right.left node becomes new root
+                        root = currentNode.getRight().getLeft();
+                        rightNode.setLeft(rightNode.getRight().getRight());
+                        // set back the saved reference of left and right nodes of root.
+                        root.setLeft(leftNode);
+                        root.setRight(rightNode);
+                    } else {
+                        if (currentNode.getValue() < parentNode.getValue()) {
+                            parentNode.setLeft(currentNode.getRight().getLeft());
+                        } else if (currentNode.getValue() > parentNode.getValue()) {
+                            parentNode.setRight(currentNode.getRight().getLeft());
+                        }
+                    }
+                }
+                return;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(9);
@@ -84,6 +153,8 @@ public class BinarySearchTree {
         System.out.println(!tree.isNull(tree.lookup(10)));
         System.out.println(!tree.isNull(tree.lookup(1)));
         System.out.println(!tree.isNull(tree.lookup(1070)));
+        tree.remove(1);
+        tree.printTree(tree.getRoot());
     }
 
 }
